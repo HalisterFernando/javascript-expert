95a27aabaf8126e7a110f4842d900acb89c266c3
Halister Fernando <halisterfernando@hotmail.com>
2024-02-24 15:51:11 -0300

    aula 01 child process

---
+++ modulo13/01-child-process/01-spawn-process/server.js
@@ -0,0 +1,15 @@
+import { createServer } from 'http';
+import { randomUUID } from 'crypto';
+import { pipeline } from 'stream/promises';
+import { createWriteStream } from 'fs'
+
+async function handler(req, res) {
+    const fileName = `file-${randomUUID()}.csv`;
+    await pipeline(
+        req,
+        createWriteStream(fileName)
+    )
+    res.end('upload with success')
+}
+
+createServer(handler).listen(3000, () => console.log('running at port 3000'))
